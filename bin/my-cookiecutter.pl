7a49a9ea8cebcd8b98adeca274919811038e3c04
Shlomi Fish <shlomif@shlomifish.org>
2023-02-28 09:32:07 +0200

    gh actions #8: db/xsl

---
+++ bin/my-cookiecutter.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+use lib './lib';
+use Shlomif::MySystem qw/ my_system /;
+
+my $src_dn =
+"$ENV{HOME}/.cookiecutters/cookiecutter--shlomif-latemp-sites/\{\{cookiecutter.project_slug\}\}";
+if ( -d $src_dn )
+{
+    my_system( [ "rsync", "-ra", "$src_dn/", "./" ], "rsync failed", );
+    require Path::Tiny;
+    Path::Tiny::path("./bin/batch-inplace-html-minifier")
+        ->edit_raw( sub { s/\n\{% (?:end)?raw %\}\n/\n/g; } );
+}
+else
+{
+    my_system(
+        [
+            'cookiecutter', '-f', '--no-input',
+            'gh:shlomif/cookiecutter--shlomif-latemp-sites',
+            'project_slug=.',
+        ],
+        'cookiecutter failed.'
+    );
+}
